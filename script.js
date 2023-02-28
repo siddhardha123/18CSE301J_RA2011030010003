96c9e3a3baf721110ad15b1ae40f3c6a20f77f0e
siddhardha123 <71877477+siddhardha123@users.noreply.github.com>
2023-02-28 15:37:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+var fill = d3.scale.category20();
+
+const submitButton = document.getElementById("submitButton");
+
+// Add a click event listener to the button
+submitButton.addEventListener("click", function() {
+  // Get the input element by its ID
+  const myInput = document.getElementById("myInput");
+
+  // Get the value of the input
+  const inputValue = myInput.value;
+  const source = inputValue
+  const stopwords = new Set("i,me,my,myself,we,us,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,whose,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,will,would,should,can,could,ought,i'm,you're,he's,she's,it's,we're,they're,i've,you've,we've,they've,i'd,you'd,he'd,she'd,we'd,they'd,i'll,you'll,he'll,she'll,we'll,they'll,isn't,aren't,wasn't,weren't,hasn't,haven't,hadn't,doesn't,don't,didn't,won't,wouldn't,shan't,shouldn't,can't,cannot,couldn't,mustn't,let's,that's,who's,what's,here's,there's,when's,where's,why's,how's,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,upon,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,say,says,said,shall".split(","))
+  const words = source.split(/[\s.]+/g)
+  .map(w => w.replace(/^[“‘"\-—()\[\]{}]+/g, ""))
+  .map(w => w.replace(/[;:.!?()\[\]{},"'’”\-—]+$/g, ""))
+  .map(w => w.replace(/['’]s$/g, ""))
+  .map(w => w.substring(0, 30))
+  .map(w => w.toLowerCase())
+  .filter(w => w && !stopwords.has(w))
+
+
+
+var layout = d3.layout.cloud()
+    .size([500, 500])
+    .words(words.map(function(d) {
+      return {text: d, size: 10 + Math.random() * 90, test: "haha"};
+    }))
+    .padding(5)
+    .rotate(function() { return ~~(Math.random() * 2) * 90; })
+    .font("Impact")
+    .fontSize(function(d) { return d.size; })
+    .on("end", draw);
+
+layout.start();
+
+function draw(words) {
+  d3.select("body").append("svg")
+      .attr("width", layout.size()[0])
+      .attr("height", layout.size()[1])
+    .append("g")
+      .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+    .selectAll("text")
+      .data(words)
+    .enter().append("text")
+      .style("font-size", function(d) { return d.size + "px"; })
+      .style("font-family", "Impact")
+      .style("fill", function(d, i) { return fill(i); })
+      .attr("text-anchor", "middle")
+      .attr("transform", function(d) {
+        return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+      })
+      .text(function(d) { return d.text; });
+}	
+
+  
+  console.log(inputValue);
+});
+
+
+
+
+
